acfe47e8a2757ce6ec4f962dbbf1c610a417d6ff
bá khanh <khanhbvh@outlook.com.vn>
2018-01-15 15:38:13 +0700

    update endpoint

--- src/app/app.setting.ts
+++ src/app/app.setting.ts
@@ -1,9 +1,7 @@
 export class AppSetting {
-    // for production
-    // public static API_ENDPOINT = 'http://api-thuha.azurewebsites.net';
-
+    public static API_ENDPOINT = '';
     // for localhost
-    public static API_ENDPOINT = 'http://localhost:59291/api/';
+    // public static API_ENDPOINT = 'http://localhost:59291/api/';
     public static pattern = {
         email: '[A-Za-z0-9]+([\\._-][A-Za-z0-9]+)*@[A-Za-z0-9]+([\\.-][A-Za-z0-9]+)*(\\.[A-Za-z]{2,4})',
         phone: '[\\d\\s]+'
