9e09f333e8a3b211857c89110cd87400eaa1107a
bá khanh <khanhbvh@outlook.com.vn>
2018-01-29 15:48:23 +0700

    update area detail

--- src/app/pages/view-area/view-area.component.ts
+++ src/app/pages/view-area/view-area.component.ts
@@ -35,14 +35,15 @@ export class ViewAreaComponent {
   private markers: Layer[] = [];
   private interval: any;
   private chartActived: boolean = false;
-  private chart = {name: '', chartData: [], options: undefined, g: undefined, value: 0, active: true};
+  private chart = { name: '', chartData: [], options: undefined, g: undefined, value: 0, active: true };
   private chartData: any[];
   private oldChartData: any[];
   private hubConnection: HubConnection;
   private sensors: any;
+  private overlay: any;
 
   constructor(private route: ActivatedRoute, private _areaService: AreaService, private state: GlobalState,
-              private _hubService: HubService, private _elementRef: ElementRef, private _sensorService: SensorService) {
+    private _hubService: HubService, private _elementRef: ElementRef, private _sensorService: SensorService) {
   }
 
   public ngOnInit() {
@@ -55,11 +56,11 @@ export class ViewAreaComponent {
 
     this.chartData = [];
     this._areaService.getById(this.areaId).subscribe(resp => {
-      this.state.notifyDataChanged('menu.activeLink', {title: 'Area Detail'});
+      this.state.notifyDataChanged('menu.activeLink', { title: 'Area Detail' });
       this.area = resp.data;
       this.sensors = resp.data.sensors || [];
       this.devices = resp.data.sensors || [];
-      this.makeMaker(this.sensors);
+      this.initMap(resp.data.photo);
 
       this.chartActived = true;
       window.dispatchEvent(new Event('resize'));
@@ -79,25 +80,31 @@ export class ViewAreaComponent {
 
   private makeMaker(sensors: any) {
     sensors.forEach((item, i) => {
-      const newMarker = marker(
-        [item.locationX, item.locationY],
-        {
-          icon: icon({
+      const newMarker = L.marker(
+        [-item.locationY, item.locationX],
+        <any>{
+          icon: L.icon({
             iconSize: [25, 41],
             iconAnchor: [13, 41],
             iconUrl: '../../../assets/img/antena_azul.png',
             shadowUrl: '../../../assets/img/marker-shadow.png',
           }),
-        },
-      ).on('click', () => {
-        this.interval && clearInterval(this.interval);
-        this.chartActived = true;
-        window.dispatchEvent(new Event('resize'));
-        setTimeout(() => {
-          this._bindGPUCharts(item.id);
+        }, )
+        .on('click', (e) => {
+          this.interval && clearInterval(this.interval);
+          this.chartActived = true;
+          window.dispatchEvent(new Event('resize'));
+          setTimeout(() => {
+            this._bindGPUCharts(item.id);
+          });
+        })
+        .on('dragend', (e: any) => {
+          const { lat, lng } = e.target.getLatLng();
+          item.locationY = -lat;
+          item.locationX = lng;
         });
-      });
       this.markers.push(newMarker);
+      this.map.addLayer(newMarker);
     });
   }
 
@@ -108,44 +115,21 @@ export class ViewAreaComponent {
     });
   }
 
-  public onMapReady(map: Map) {
-    this.map = map;
-    if (this.devices && this.devices.length) {
-      map.panTo([this.devices[0].locationX, this.devices[0].locationY]);
-    } else {
-      this._areaService.getById(this.areaId).subscribe(resp => {
-        // for production
-        if (resp.data.sensors.length > 0) {
-          const maxLocX = Math.max.apply(Math, resp.data.sensors.map(function (o) {
-            return o.locationX;
-          }));
-          const maxLocY = Math.max.apply(Math, resp.data.sensors.map(function (o) {
-            return o.locationY;
-          }));
-          // for test
-          map.panTo([maxLocX, maxLocY]);
-          map.setZoom(9);
-        }
-      });
-    }
-  }
-
   private _bindGPUCharts(i) {
     function legendFormatter(data) {
-      if (data.x === null) {
+      if (data.x == null) {
         // This happens when there's no selection and {legend: 'always'} is set.
-        return `<br>${data.series.map(function (series) {
-          return series.dashHTML + ' ' + series.labelHTML;
-        }).join('<br>')}`;
+        return '<br>' + data.series.map(function (series) {
+          return series.dashHTML + ' ' + series.labelHTML
+        }).join('<br>');
       }
 
-      const d = new Date(Date.parse(data.xHTML));
-      const datestring = `${('0' + d.getDate()).slice(-2)}-${('0' + (d.getMonth() + 1)).slice(-2)}-${d.getFullYear()} ${('0' + d.getHours()).slice(-2)}:${('0' + d.getMinutes()).slice(-2)}:${('0' + d.getSeconds()).slice(-2)}`;
+      let d = new Date(Date.parse(data.xHTML));
+      let datestring = ("0" + d.getDate()).slice(-2) + "-" + ("0" + (d.getMonth() + 1)).slice(-2) + "-" +
+        d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2) + ":" + ("0" + d.getSeconds()).slice(-2);
       let html = this.getLabels()[0] + ': ' + datestring;
       data.series.forEach(function (series) {
-        if (!series.isVisible) {
-          return;
-        }
+        if (!series.isVisible) return;
         let labeledData = series.labelHTML + ': ' + series.yHTML;
         if (series.isHighlighted) {
           labeledData = '<b>' + labeledData + '</b>';
@@ -153,6 +137,7 @@ export class ViewAreaComponent {
         html += '<br>' + series.dashHTML + ' ' + labeledData;
       });
       return html;
+
     }
 
     const time = Date();
@@ -173,7 +158,7 @@ export class ViewAreaComponent {
       labelsDiv: this._elementRef.nativeElement.querySelector('.chart-legend'),
       legendFormatter,
       series: {
-        'Accelerometer': {axis: 'y'},
+        'Accelerometer': { axis: 'y' },
       },
       axes: {
         y: {
@@ -189,10 +174,10 @@ export class ViewAreaComponent {
     this.interval = setInterval(() => {
       this.chart.value = this.chartData && this.chartData[i] && this.chartData[i].value || 0;
       this.chart.chartData.push([new Date(), this.chart.value]);
-      this.chart.g.updateOptions({'file': this.chart.chartData});
+      this.chart.g.updateOptions({ 'file': this.chart.chartData });
       this.chart.options.dateWindow[0] += 500;
       this.chart.options.dateWindow[1] += 500;
-      this.chart.g.updateOptions({'dateWindow': this.chart.options.dateWindow});
+      this.chart.g.updateOptions({ 'dateWindow': this.chart.options.dateWindow });
     }, 1000);
 
     this.getData(i, time);
@@ -216,16 +201,61 @@ export class ViewAreaComponent {
     window.dispatchEvent(new Event('resize'));
   }
 
+  private initMap(photoUrl) {
+    if (!photoUrl) {
+      return;
+    }
+    (new Promise((resolve) => {
+      const img = new Image();
+      img.src = photoUrl;
+      img.onload = function () {
+        resolve({
+          width: (this as any).width,
+          height: (this as any).height,
+        });
+      };
+    }).then((size: any) => {
+      this.map = new L.Map('map', <any>{
+        // set map center to center of floor image
+        center: L.latLng(-size.height / 2, size.width / 2),
+        crs: L.CRS.Simple,
+        maxZoom: 4,
+        minZoom: -2,
+        attributionControl: false,
+        zoomSnap: 0,
+        contextmenu: true,
+        contextmenuWidth: 140,
+        contextmenuItems: [{
+          text: 'Add Sensor',
+          callback: () => {
+          }
+        }]
+      },
+      );
+      // bound by width and height of floor
+      const bounds = L.latLngBounds(L.latLng(-size.height, 0), L.latLng(0, size.width));
+      this.overlay = L.imageOverlay(photoUrl, bounds).addTo(this.map);
+      this.map.fitBounds(bounds);
+      this.map.setMaxBounds(bounds.pad(0.5));
+      window.dispatchEvent(new Event('resize'));
+      this.makeMaker(this.area.sensors);
+    }));
+  }
+
   public onChange(sensorType) {
-    if (sensorType === -1) {
+    if (sensorType == -1) {
       this.devices = this.sensors;
     } else {
       this.devices = this.sensors.filter(obj => {
-        return obj.sensorType.id === sensorType;
+        return obj.sensorType.id == sensorType;
       });
     }
-
+    this.markers.forEach(obj => {
+      this.map.removeLayer(obj);
+    })
     this.markers = [];
+
     this.makeMaker(this.devices);
   }
+
 }
